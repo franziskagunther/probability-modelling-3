5f977135790def124a260b9317655179956a3703
Franziska Günther <Franziska@MacBook-Air-5.fritz.box>
2020-04-29 22:47:52 +0200

    Minor modifications (addition instead of modification)

--- pmex-3.R
+++ pmex-3.R
@@ -3,7 +3,9 @@
 dat = read.csv(file = "ml-ex-3.csv")
 
 neglogLikelihood <- function(theta, obs) {
-  w = theta[1:3]
+  w_1 = theta[1]
+  w_2 = theta[2]
+  w_3 = theta[3]
   beta_1 = theta[4]
   beta_2 = theta[5]
   n = obs[1] 
@@ -11,13 +13,13 @@ neglogLikelihood <- function(theta, obs) {
   y = obs[1+c((n+1):(2*n))] 
   x = c(55, 425, 519) 
   x_multi = c(163, 712, 625) 
-  logF = dmultinom(x=c(x_multi[1], x_multi[2], x_multi[3]), size=1500, prob=c(w[1], w[2], w[3]), log=TRUE) * dbinom(x=x[g+1], size=x_multi[g+1], prob=(1/(1+exp(-(beta_1 + beta_2*g)))), log=TRUE)
+  logF = dmultinom(x=c(x_multi[1], x_multi[2], x_multi[3]), size=1500, prob=c(w_1, w_2, w_3), log=TRUE) + dbinom(x=x[g+1], size=x_multi[g+1], prob=(1/(1+exp(-(beta_1 + beta_2*g)))), log=TRUE)
   return(-sum(logF)) 
 }
 
 n <- length(dat$G)
 obs <- c(n, dat$G, dat$Y)
-theta_init = c(c(0.1, 0.5, 0.4), c(0, 0)) 
+theta_init = c(c(0.1, 0.5, 0.4), c(1, 1)) 
 
-out <- optim(theta_init, neglogLikelihood, gr = NULL, obs, method = "L-BFGS-B", lower = c(0.01, 0.01, 0.01, -1000, -1000), upper = c(0.99, 0.99, 0.99, 1000, 1000))
+out <- optim(theta_init, neglogLikelihood, gr = NULL, obs, method = "L-BFGS-B", lower = c(0.01, 0.01, 0.01, 0.001, 0.001), upper = c(0.99, 0.99, 0.99, 1000, 1000))
 
