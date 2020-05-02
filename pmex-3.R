3cf301f176526f2241dc049f7d4b86381192765e
Franziska Günther <Franziska@MacBook-Air-5.fritz.box>
2020-05-02 12:40:47 +0200

    Corrections: logF variable and restriction multinomial parameters to sum to 1

--- pmex-3.R
+++ pmex-3.R
@@ -8,16 +8,13 @@ neglogLikelihood <- function(theta, obs) {
   w_3 = theta[3]
   beta_1 = theta[4]
   beta_2 = theta[5]
+  l = 10^7
   n = obs[1] 
   g = obs[1+c(1:n)] 
   y = obs[1+c((n+1):(2*n))] 
-  x = c(55, 425, 519) 
-  g_0 = length(g[g == 0])
-  g_1 = length(g[g == 1])
-  g_2 = length(g[g == 2])
-  y_1 = c(length(y[g == 0][y[g == 0] == 1]), length(y[g == 1][y[g == 1] == 1]), length(y[g == 2][y[g == 2] == 1]))
-  logF = dmultinom(x=c(g_0, g_1, g_2), size=1500, prob=c(w_1, w_2, w_3), log=TRUE) + dbinom(x=y_1[g+1], size=c(g_0, g_1, g_2)[g+1], prob=(1/(1+exp(-(beta_1 + beta_2*g)))), log=TRUE)  
-  return(-sum(logF)) 
+  wG = 1/(1+exp(-(beta_1 + beta_2*g)))
+  logF = sum( (g==0)*log(w_1)+(g==1)*log(w_2)+(g==2)*log(w_3) + y*log(wG) + (1-y)*log(1-wG) )
+  return(-sum(logF) + l*(w_1+w_2+w_3-1)^2) 
 }
 
 n <- length(dat$G)
@@ -29,7 +26,7 @@ out <- optim(theta_init, neglogLikelihood, gr = NULL, obs, method = "L-BFGS-B", 
 w_g_0 <- (1/(1+exp(-(out$par[4] + out$par[5]*0))))
 w_g_1 <- (1/(1+exp(-(out$par[4] + out$par[5]*1))))
 w_g_2 <- (1/(1+exp(-(out$par[4] + out$par[5]*2))))
-
-cat(w_g_0, "w_g for socioeconomic status 0")
-cat(w_g_1, "w_g for socioeconomic status 1")
-cat(w_g_2, "w_g for socioeconomic status 2")
+ 
+cat(w_g_0, "w_g for socioeconomic status 0 \n")
+cat(w_g_1, "w_g for socioeconomic status 1 \n")
+cat(w_g_2, "w_g for socioeconomic status 2 \n")
